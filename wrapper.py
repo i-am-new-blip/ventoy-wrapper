3fa1a167baa498dae0d441a3e779e33c9a5ca2d0
i-am-new-blip <newaccount123321333@proton.me>
2026-08-23 14:18:42 -0300

    Update target path to use dynamic base name

--- wrapper.py
+++ wrapper.py
@@ -1,10 +1,10 @@
-#![shabang]
+%s
 import os
 from path import Path
 import sys
 
 base = Path.home() / "ventoy"
-target = base / "baseexec"
+target = base / "%s"
 
 os.chdir(base)
 os.execv(target, [str(target), *sys.argv[1:]])
