772ccfa0b650ceea3335c9b8df11100f2891be96
i-am-new-blip <newaccount123321333@proton.me>
2026-08-23 14:58:21 -0300

    Implement updater verification in wrapper.py
    Add updater verification before changing directory.

--- wrapper.py
+++ wrapper.py
@@ -5,5 +5,9 @@ import sys
 base = Path.home() / "ventoy"
 target = base / "%s"
 
+if globals().get('updater_verify',True):
+  import updater
+  updater.updater_check()
+
 os.chdir(base)
 os.execv(target, [str(target), *sys.argv[1:]])
