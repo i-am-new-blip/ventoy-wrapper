903788e57c38a676459a9081a95dc70ebe663217
i-am-new-blip <newaccount123321333@proton.me>
2026-08-23 15:00:02 -0300

    Refactor main function and improve readability

--- wrapper.py
+++ wrapper.py
@@ -2,14 +2,12 @@
 from path import Path
 import sys
 
-def main():
-  base = Path.home() / "ventoy"
-  target = base / "%s"
+base = Path.home() / "ventoy"
+target = base / "%s"
   
-  if globals().get('updater_verify',True):
+if globals().get('updater_verify',True):
     import updater
     updater.updater_check()
   
-  os.chdir(base)
-  os.execv(target, [str(target), *sys.argv[1:]])
-if __name__ == '__main__': main()
+os.chdir(base)
+os.execv(target, [str(target), *sys.argv[1:]])
