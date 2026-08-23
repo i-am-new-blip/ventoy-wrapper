2b63b1fbdc0e3ded98650f9ada11910658114cbf
i-am-new-blip <newaccount123321333@proton.me>
2026-08-23 19:58:12 -0300

    Release v1.1.81

--- installer.py
+++ installer.py
@@ -253,7 +253,7 @@ def install():
     if get_os() == "linux":
         shebang = f"#!{sys.executable}\n"
         path = f"VentoyGUI.{get_arch()}"
-        base = "/ \"opt\""
+        base = "Path("/opt/ventoy")"
     else:
         path = "Ventoy2Disk.exe"
 
