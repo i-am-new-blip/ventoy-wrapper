46e3f4ed3d1a69a2bd5545237b2533a875d955ba
i-am-new-blip <newaccount123321333@proton.me>
2026-08-23 19:28:25 -0300

    Release v1.1.4

--- wrapper.py
+++ wrapper.py
@@ -6,9 +6,8 @@ import sys
 base = Path.home() / "ventoy"
 target = base / "%s"
   
-if globals().get('updater_verify',True):
-    import updater
-    updater.updater_check()
+import updater
+updater.updater_check()
   
 os.chdir(base)
 os.execv(target, [str(target), *sys.argv[1:]])
