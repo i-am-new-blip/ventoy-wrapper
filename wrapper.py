dbaf932e36a44802b588ef4d9795623583aa78fe
i-am-new-blip <newaccount123321333@proton.me>
2026-08-23 14:59:00 -0300

    Refactor code into main function for clarity

--- wrapper.py
+++ wrapper.py
@@ -2,12 +2,14 @@
 from path import Path
 import sys
 
-base = Path.home() / "ventoy"
-target = base / "%s"
-
-if globals().get('updater_verify',True):
-  import updater
-  updater.updater_check()
-
-os.chdir(base)
-os.execv(target, [str(target), *sys.argv[1:]])
+def main():
+  base = Path.home() / "ventoy"
+  target = base / "%s"
+  
+  if globals().get('updater_verify',True):
+    import updater
+    updater.updater_check()
+  
+  os.chdir(base)
+  os.execv(target, [str(target), *sys.argv[1:]])
+if __name__ == '__main__': main()
