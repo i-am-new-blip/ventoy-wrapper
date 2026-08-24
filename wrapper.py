51a3f387756577e33cb3f5a0f24c8310404081e8
i-am-new-blip <newaccount123321333@proton.me>
2026-08-23 21:55:36 -0300

    Release v1.2.5

--- wrapper.py
+++ wrapper.py
@@ -11,7 +11,32 @@ updater.updater_check()
   
 os.chdir(base)
 if os.name == "nt":
-    import subprocess
+    import subprocess, ctypes
+    
+    if not ctypes.windll.shell32.IsUserAnAdmin():
+        print(
+            f"\033[33mAsking for elevation because on {get_os()} will need "
+            f"to store @ {elevated_path}, which needs elevation."
+            "\033[0m"
+        )
+        
+        params = subprocess.list2cmdline(sys.argv)
+
+        result = ctypes.windll.shell32.ShellExecuteW(
+            None,
+            "runas",
+            sys.executable,
+            params,
+            None,
+            1,
+        )
+
+        if result <= 32:
+            raise RuntimeError("Failed to request administrator privileges")
+
+        sys.exit()
+        
+    
     subprocess.Popen(
         [str(target), *sys.argv[1:]],
         cwd=base,
